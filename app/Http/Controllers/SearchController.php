33c1ab80a641ce6b29a4e37aca92432e6ce01bc5
mdalifkhanrifat <mdalifkhanrifat@gmail.com>
2023-07-14 17:50:13 +0600

    Update Search Query And Readme File

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -7,8 +7,6 @@ use Illuminate\Http\Request;
 use Illuminate\View\View;
 use Illuminate\Support\Facades\Auth;
 
-
-
 class SearchController extends Controller
 {
     public function view(Request $request)
@@ -20,19 +18,22 @@ class SearchController extends Controller
         $user = Auth::user();
         $users = $user->id;
 
-       // dd($search);
-
         if ($search == "") {
 
             return redirect('/contract')->with('massage', 'No data available');
         } else {
-            $contacts = Contact::where('user_id', $users, 'and')->where('name', 'LIKE', '%' . $search . '%')->orWhere('mobile', 'LIKE', '%' . $search . '%')->orWhere('email', 'LIKE', '%' . $search . '%')->orWhere('group', 'LIKE', '%' . $search . '%')->paginate(10);
+
+            $contacts = Contact::where('user_id', $users)
+                ->where(function ($query) use ($search) {
+                    $query->where('name', 'LIKE', '%' . $search . '%')
+                        ->orWhere('mobile', 'LIKE', '%' . $search . '%')
+                        ->orWhere('email', 'LIKE', '%' . $search . '%')
+                        ->orWhere('email', 'LIKE', '%' . $search . '%');
+                })->paginate(10);
+
+
             return view('index', compact('contacts'));
         }
-
-
-        // dd($contacts);
-
 
     }
 }
